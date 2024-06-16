52f8e91ac47a3ab2ed7e58d7cabb34f4a168e110
Pramodi Peshila <168624180+PramodiPeshila@users.noreply.github.com>
2024-06-16 23:03:21 +0530

    Add files via upload

---
+++ Practical.scala
@@ -0,0 +1,12 @@
+
+def Area(x:Double)=Math.PI*x*x
+
+def fahrenheit(c:Double)=c*1.8000+32.00
+
+def volume(r:Double)=Math.PI*4/3*r*r*r
+
+def cost()=24.95*60/100 + 3 + 0.75*10
+
+def time()=8*2 + 3*7 + 2*8
+
+
