ff13bc103a8371b32057267100baa13ff317422e
Pramodi Peshila <168624180+PramodiPeshila@users.noreply.github.com>
2024-06-17 20:48:13 +0530

    Update Practical.scala

--- Practical.scala
+++ Practical.scala
@@ -5,7 +5,15 @@ def fahrenheit(c:Double)=c*1.8000+32.00
 
 def volume(r:Double)=Math.PI*4/3*r*r*r
 
-def cost()=24.95*60/100 + 3 + 0.75*10
+def Price(x:Int)=x*24.95*0.6
+def shipping(x:Int)={
+	if(x>50){
+		3+(x-50)*0.75
+	}
+	else{
+		3
+	}
+}
 
 def easy(x:Int):Int=x*8
 def tempo(x:Int):Int=x*7
