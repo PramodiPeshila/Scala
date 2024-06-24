3b7cf7e4c206ae4cd90fbe807e51f43f655959ac
Pramodi Peshila <168624180+PramodiPeshila@users.noreply.github.com>
2024-06-24 21:48:23 +0530

    Add files via upload

---
+++ Practical-2/1.scala
@@ -0,0 +1,23 @@
+
+   
+  val i: Int = 2
+  val k: Int = 2
+  val j: Int = 2
+  val m: Int = 5
+  val n: Int = 5
+  val f: Float = 12.0F
+  val g: Float = 4.0F
+  val c: Char = 'X'
+
+  def A: Int = k + 12 * m
+  def B: Double = m / j
+  def C: Int = n % j
+  def D: Int = (m / j) * j
+  def E: Double = f + 10 * 5 + g
+
+  var iNew : Int = i + 1
+	
+  def F: Double = iNew * n
+
+//Java can execute increments and  decrements.But Scala Cannot
+
